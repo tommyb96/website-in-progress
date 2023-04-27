950a6311d37e16e47c7103e6668571d3cd606a19
Anna Hoehl <anna.hoehl@curanta.de>
2023-04-27 15:57:01 +0200

    make logo responsive

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ function App() {
     <>
       <GlobalStyles />
       <Logo src={logo} alt="Logo" />
+      <CompanyName>
+        <span style={{ color: "rgb(0, 0, 255)", fontSize: "35px" }}>
+          modern mind
+        </span>{" "}
+        recruitment
+      </CompanyName>
+      <ErsterAbsatz>
+        Sind Sie auf der Suche nach qualifizierten{" "}
+        <StyledSpan>Mitarbeitern</StyledSpan> aus dem Ausland, um Ihre offenen
+        Stellen erfolgreich zu besetzen?
+      </ErsterAbsatz>
+      <ZweiterAbsatz>
+        {" "}
+        Dann sind Sie bei uns genau richtig! Unser erfahrenes Team arbeitet
+        derzeit intensiv an einem Workshop zur Gewinnung internationaler
+        Fachkräfte mit Fokus auf Drittstaaten. Dieser Workshop basiert auf
+        jahrelanger Erfahrung im Bereich Fachkräftegewinnung aus dem Ausland und
+        vermittelt Ihnen alle notwendigen Informationen sowie Tipps und Tricks
+        für maximalen Erfolg.
+      </ZweiterAbsatz>
     </>
   );
 }
@@ -16,4 +36,33 @@ export default App;
 const Logo = styled.img`
   width: 150px;
   margin: 25px;
+
+  @media (max-width: 768px) {
+    width: 80px;
+  }
+`;
+
+const CompanyName = styled.div`
+  font-family: "Comfortaa", cursive;
+  color: rgb(0, 167, 155);
+  font-size: 30px;
+`;
+
+const ErsterAbsatz = styled.div`
+  padding: 10px 10vw 10px 10vw;
+  max-width: 500px;
+  font-size: 23px;
+  font-family: futura-pt, sans-serif;
+  font-weight: 500;
+  font-style: normal;
+`;
+const StyledSpan = styled.span`
+  font-family: "Righteous", cursive;
+  color: rgb(0, 0, 255);
+`;
+
+const ZweiterAbsatz = styled.div`
+  font-size: 20px;
+  max-width: 500px;
+  padding: 10px 10vw 10px 10vw;
 `;
--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -3,7 +3,9 @@ import { createGlobalStyle } from "styled-components";
 export default createGlobalStyle`
 * {
 margin: 0;
-background-color:rgba(0, 167, 155);
-
+background-color:rgba(220,233, 252);
+font-family:  futura-pt, sans-serif;
+font-weight: 300;
+font-style: normal;;
 
 }`;
